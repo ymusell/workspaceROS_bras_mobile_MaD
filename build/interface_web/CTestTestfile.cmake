94de76997996c5b04166cee74bc77b7e5ca777fa
ymusell <ymusellec@laposte.net>
2021-11-25 18:08:26 +0100

    fin de semaine mais le dossier entier

---
+++ build/interface_web/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yann/catkin_ws/src/interface_web
+# Build directory: /home/yann/catkin_ws/build/interface_web
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
